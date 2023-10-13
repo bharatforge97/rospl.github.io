060b095a695489a55126c19e9e7381f21a5c74d8
bharatforge97 <147376680+bharatforge97@users.noreply.github.com>
2023-10-13 12:56:12 +0530

    Update errorMiddleware.js

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : statusCode
-    res.Status(statusCode);
+    res.status(statusCode);
     res.json({
         message: err.message,
         stack: process.env.NODE_ENV === 'Production' ? null : err.stack
